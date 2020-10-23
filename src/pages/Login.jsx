c0d3d210e5f2856dd066a77a5d7b16ad71103ae3
arun kumar <alekhya0801@gmail.com>
2020-10-23 10:18:08 +0530

    Added : toogle password visibility

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import { Button, TextField, Typography } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Typography,
+  FormControl,
+  IconButton,
+  OutlinedInput,
+  InputLabel,
+  InputAdornment,
+} from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Logo from "../components/Logo";
 import "../css/login.css";
 import UserService from "../services/user/UserService";
@@ -7,17 +18,23 @@ function Login() {
   const [values, setValues] = useState({
     emailId: "",
     password: "",
+    showPassword: false,
   });
 
   function handleOnChange(e) {
     setValues({ ...values, [e.currentTarget.name]: e.currentTarget.value });
   }
 
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
   function userLogin() {
     const data = {
       email: values.emailId,
       password: values.password,
     };
+    console.log(data.email + " " + data.password);
     UserService.userLogin(data)
       .then((data) => {
         console.log(data);
@@ -40,17 +57,35 @@ function Login() {
         <TextField
           label="Email"
           name="emailId"
+          id="outlined-basic"
           onChange={handleOnChange}
           variant="outlined"
           fullWidth={true}
         />
-        <TextField
-          label="Password"
-          name="password"
-          onChange={handleOnChange}
-          variant="outlined"
-          fullWidth={true}
-        />
+        <FormControl fullWidth variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            label="Password"
+            name="password"
+            type={values.showPassword ? "text" : "password"}
+            onChange={handleOnChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            fullWidth={true}
+          />
+        </FormControl>
         <div>
           <a href="#">Create account</a>
           <Button
