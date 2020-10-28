d9b9dcdef08f48f36f05ec86cd6ed8695754a676
arun kumar <alekhya0801@gmail.com>
2020-10-28 15:05:30 +0530

    Added : tool tip for color pallet icon

--- src/components/ColorPalletIcon.jsx
+++ src/components/ColorPalletIcon.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { IconButton } from "@material-ui/core";
 import PaletteOutlinedIcon from "@material-ui/icons/PaletteOutlined";
+import Tooltip from "@material-ui/core/Tooltip";
 
 function ColorPalletIcon(props) {
   return (
-    <IconButton className={props.buttonClassName}>
-      <PaletteOutlinedIcon className={props.iconClassName} />
-    </IconButton>
+    <Tooltip title="Change colour" placement="bottom">
+      <IconButton className={props.buttonClassName}>
+        <PaletteOutlinedIcon className={props.iconClassName} />
+      </IconButton>
+    </Tooltip>
   );
 }
 
